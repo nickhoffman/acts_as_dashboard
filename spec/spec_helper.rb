7c45097245ee723515f4d485edea9fd7d4a1869e
Nick Hoffman <nick@deadorange.com>
2010-04-18 12:41:23 -0400

    Fixed spec_helper.rb so that it loads the Rails application that ActsAsDashboard is being tested within.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'acts_as_dashboard'
-require 'spec'
-require 'spec/autorun'
+# Require the Rails application's spec_helper.
+require File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'spec', 'spec_helper')
 
 Spec::Runner.configure do |config|
 end
+
+require 'acts_as_dashboard'
