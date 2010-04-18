1fc5168a975a9e1e91ecdacd6d6996ab3a1cf034
Nick Hoffman <nick@deadorange.com>
2010-04-18 15:38:03 -0400

    Moved a spec around to stay consistent in how other specs are written.

--- spec/acts_as_dashboard/config_spec.rb
+++ spec/acts_as_dashboard/config_spec.rb
@@ -1,15 +1,17 @@
 require File.join File.dirname(__FILE__), '..', 'spec_helper'
 
 describe ActsAsDashboard::Config do
-  it 'makes its "widgets" instance variable readable' do
-    config = ActsAsDashboard::Config.new
-    config.widgets.should equal config.instance_variable_get(:@widgets)
-  end
-
-  describe 'initialization' do
+  describe 'initialization' do # {{{
     it 'creates an array of widgets' do
       config = ActsAsDashboard::Config.new
       config.instance_variable_get(:@widgets).should == []
     end
-  end
+  end # }}}
+
+  describe 'attributes' do # {{{
+    it 'returns the "widgets" attribute' do
+      config = ActsAsDashboard::Config.new
+      config.widgets.should equal config.instance_variable_get(:@widgets)
+    end
+  end # }}}
 end
