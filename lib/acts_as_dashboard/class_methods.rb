0868f9a8177760125acc4d9222fbcc511c2e67d1
Nick Hoffman <nick@deadorange.com>
2010-04-18 13:18:08 -0400

    Calling #acts_as_dashboard creates a AAD::Config object.

--- lib/acts_as_dashboard/class_methods.rb
+++ lib/acts_as_dashboard/class_methods.rb
@@ -1,6 +1,8 @@
 module ActsAsDashboard
   module ClassMethods
     def acts_as_dashboard
+      @dashboard_config = ActsAsDashboard::Config.new
+
       include InstanceMethods
     end
   end
--- spec/acts_as_dashboard/class_method_specs.rb
+++ spec/acts_as_dashboard/class_method_specs.rb
@@ -1,7 +1,12 @@
 require File.join File.dirname(__FILE__), '..', 'spec_helper'
 
 describe ActsAsDashboard::ClassMethods do
-  describe '#acts_as_dashboard' do
+  describe '#acts_as_dashboard' do # {{{
+    before :each do
+      @config = mock ActsAsDashboard::Config
+      ActsAsDashboard::Config.stub(:new).and_return @config
+    end
+
     it 'includes ActsAsDashboard::InstanceMethods' do
       class FoosController < ApplicationController
         acts_as_dashboard
@@ -9,5 +14,13 @@ describe ActsAsDashboard::ClassMethods do
 
       FoosController.included_modules.should include ActsAsDashboard::InstanceMethods
     end
-  end
+
+    it 'creates a Config object' do
+      ActsAsDashboard::Config.should_receive(:new).and_return @config
+
+      class FoosController < ApplicationController
+        acts_as_dashboard
+      end
+    end
+  end # }}}
 end
