8f53bc05d7446dd5c7d467322c609ec9137e5d36
Nick Hoffman <nick@deadorange.com>
2010-04-18 15:41:26 -0400

    Widgets can be added to ActsAsDashboard::Config .

--- lib/acts_as_dashboard/config.rb
+++ lib/acts_as_dashboard/config.rb
@@ -5,5 +5,10 @@ module ActsAsDashboard
     def initialize
       @widgets = []
     end
+
+    def add_widget(widget)
+      raise ArgumentError, 'The "widget" argument must be an ActsAsDashboard::Widget.' unless widget.is_a? ActsAsDashboard::Widget
+      @widgets.push widget
+    end
   end
 end
--- spec/acts_as_dashboard/config_spec.rb
+++ spec/acts_as_dashboard/config_spec.rb
@@ -14,4 +14,22 @@ describe ActsAsDashboard::Config do
       config.widgets.should equal config.instance_variable_get(:@widgets)
     end
   end # }}}
+
+  describe 'adding a widget' do
+    before :each do
+      @config = ActsAsDashboard::Config.new
+    end
+
+    it 'raises an exception if given an invalid value' do
+      Proc.new {@config.add_widget nil}.should raise_error ArgumentError, 'The "widget" argument must be an ActsAsDashboard::Widget.'
+    end
+
+    it 'is successful when given an ActsAsDashboard::Widget' do
+      widget = ActsAsDashboard::Widget.new
+
+      @config.widgets.should == []
+      @config.add_widget widget
+      @config.widgets.should == [widget]
+    end
+  end
 end
