a6c1aea6b81ffd7668890517796bacb92ac3775a
Nick Hoffman <nick@deadorange.com>
2010-04-18 21:25:11 -0400

    AAD::InstanceMethods defines the "show" action for dashboard controllers.

--- lib/acts_as_dashboard/instance_methods.rb
+++ lib/acts_as_dashboard/instance_methods.rb
@@ -1,4 +1,16 @@
 module ActsAsDashboard
   module InstanceMethods
+    def show
+      @widget_names = dashboard_config.widgets.map {|w| w.name}.join ', '
+
+      render :file => File.join(File.dirname(__FILE__), 'app', 'views', 'dashboards', 'show.haml')
+    end
   end
+
+  protected
+
+    # Make the ActsAsDashboard::Config instance variable easily accessible.
+    def dashboard_config
+      self.class.dashboard_config
+    end
 end
--- spec/acts_as_dashboard/instance_methods_spec.rb
+++ spec/acts_as_dashboard/instance_methods_spec.rb
@@ -1,5 +1,11 @@
 require File.join File.dirname(__FILE__), '..', 'spec_helper'
 
 describe ActsAsDashboard::InstanceMethods do
-  it 'does nothing yet'
+  it 'creates the "show" action for the controller' do
+    class DashboardsController < ApplicationController
+      acts_as_dashboard {}
+    end
+
+    DashboardsController.action_methods.should include 'show'
+  end
 end
