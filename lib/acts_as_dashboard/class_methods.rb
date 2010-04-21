a80332d54a7a5b3c875610178de84d3de015f973
Nick Hoffman <nick@deadorange.com>
2010-04-20 20:52:10 -0400

    Well, it turns out that the last commit doesn't actually result in Rails recognizing the new routes.  =(

--- lib/acts_as_dashboard/class_methods.rb
+++ lib/acts_as_dashboard/class_methods.rb
@@ -9,10 +9,12 @@ module ActsAsDashboard
 
       controller_name = self.to_s.underscore.sub('_controller', '').singularize
 
-      ActionController::Routing::Routes.draw do |map|
-        map.resource  controller_name.to_sym, :only => :show
-        map.connect   "#{controller_name}/widgets/*path", :controller => controller_name.pluralize, :action => 'widget_data'
-      end
+#     # This is commented out because I haven't figured out how to tell Rails to
+#     # actually recognize the new routes that are being created.
+#     ActionController::Routing::Routes.draw do |map|
+#       map.resource  controller_name.to_sym, :only => :show
+#       map.connect   "#{controller_name}/widgets/*path", :controller => controller_name.pluralize, :action => 'widget_data'
+#     end
     end
 
     def dashboard_number(&block)
